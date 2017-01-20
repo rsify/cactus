1d2e58a6079289efc993ec5b8f9d7965f1629e97
nikersify <nikersifyyt@gmail.com>
2017-01-20 21:26:03 +0100

    stuff and things

--- app.js
+++ app.js
@@ -11,12 +11,31 @@ sock.on('connect', (e) => {
 	sock.write('login serveradmin pIqmSeQI\n')
 	sock.write('use 1\n')
 	sock.write('clientupdate client_nickname=botty\n')
-	sock.write('sendtextmessage target=1 targetmode=1 msg=hello\\sworld\n')
-	sock.write('servernotifyregister event=channel\n')
-	sock.write('servernotifyregister event=textprivate\n')
-	sock.write('servernotifyregister event=textchannel\n')
-	sock.write('servernotifyregister event=textserver\n')
-	sock.write('servernotifyregister event=server\n')
+	sock.write('sendtextmessage target=2 targetmode=1 msg=hello\\sworld\n')
+
+	// (can't know which channel event originated from)
+	// sock.write('servernotifyregister event=channel id=1\n') // channel switching, id required
+	// notifyclientmoved ctid=2 reasonid=0 clid=3
+
+	// sock.write('servernotifyregister event=textprivate\n') // all private msg, including your own!!11
+	// notifytextmessage targetmode=1 msg=je target=13 invokerid=3 invokername=Niker invokeruid=lugH8CEMs26SceRderQ4J1NX1fI=
+
+	// (can't know which channel message was in)
+	// sock.write('servernotifyregister event=textchannel id=1\n') // message in channel, id required
+	// notifytextmessage targetmode=2 msg=y invokerid=3 invokername=Niker invokeruid=lugH8CEMs26SceRderQ4J1NX1fI=
+
+	// sock.write('servernotifyregister event=textserver\n') // messages in global channel
+	// notifytextmessage targetmode=3 msg=testtt invokerid=3 invokername=Niker invokeruid=lugH8CEMs26SceRderQ4J1NX1fI=
+
+	// sock.write('servernotifyregister event=server\n') // server events, client
+	// notifyclientleftview cfid=1 ctid=0 reasonid=8 reasonmsg=leaving clid=3
+	// notifycliententerview cfid=0 ctid=1 reasonid=0 clid=2 client_unique_identifier=lugH8CEMs26SceRderQ4J1NX1fI= client_nickname=Niker client_input_muted=0...
+
+	// events:
+	// notifyclientmoved
+	// notifyclientleftview
+	// notifycliententerview
+	// notifytextmessage
 
 	setInterval(() => {
 		sock.write('\n\r')
--- new.js
+++ new.js
@@ -4,6 +4,4 @@ const Cactus = require('./lib/cactus')
 
 let bot = new Cactus().start()
 
-
-
 bot.send('use 1', Cactus.log)
